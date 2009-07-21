d1eb5bed9d08a1676969cc401ed60181b9dc068e
David Richards <davidlamontrichards@gmail.com>
2009-07-21 13:37:57 -0600

    Initial commit to just_enumerable_stats.

---
+++ features/support/env.rb
@@ -0,0 +1,11 @@
+$LOAD_PATH.unshift(File.dirname(__FILE__) + '/../../lib')
+require 'just_enumerable_stats'
+
+require 'spec/expectations'
+
+require 'test/unit/assertions'
+
+World do |world|
+  
+  world
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,8 @@
+$: << File.join(File.dirname(__FILE__), "/../lib") 
+require 'rubygems' 
+require 'spec' 
+require 'just_enumerable_stats'
+
+Spec::Runner.configure do |config|
+  
+end
